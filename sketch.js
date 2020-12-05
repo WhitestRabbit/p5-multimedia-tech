6a24b98adb2e07832248c60919daf2f43183c351
WhitestRabbit <stefanosmixalas@gmail.com>
2020-12-05 16:09:21 +0200

    images, alterations, extra shape, comments

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
-let img;
-let button;
-let a = 0;
+let imgB, imgS, button, a = 0;
 
 function setup() {
+  //creation of the canvas and the image, paragraph and button elements
     createCanvas(windowWidth, windowHeight, WEBGL);
-    img = loadImage('assets/img.jpg');
+    imgB = loadImage('assets/rosco.jpg');
+    imgS = loadImage('assets/miguel.jpg');
     p = createP('Lights Out!');
     p.position(width/2.1,height*0.08);
-    console.log(p);
     btn = createButton('Light Toggler');
     btn.position(width/2.1,height*0.92);
     btn.mousePressed(lightToggle);
   }
+
   function draw() {
+    //sets the color grade of the background
     background(30);
 
     let locX = mouseX - width / 2;
@@ -20,6 +21,7 @@ function setup() {
     let dirX = (mouseX / width - 0.5) * 2;
     let dirY = (mouseY / height - 0.5) * 2;
 
+    //depending on the value of the variable a, that changes whenever the button is pressed, the conditional changes the type of lighting and sets the text of the paragraph accordingly
     switch(a) {
       case 0: 
         ambientLight(0);
@@ -30,25 +32,38 @@ function setup() {
         p.elt.innerText = "Directional Light";
         break;
       case 2:
-        pointLight(255, 255, 255, locX, locY, 200);
+        pointLight(255, 255, 255, locX, locY, 250);
         p.elt.innerText = "Point Light";
         break;
     }
     
+    //creation of the first 3d shape, a box: sets its position, sets it to rotate on all axes, its size and image texture
+    //push and pop stores these drawing style settings so that we can alter them later for other elements
+    push();
+    translate(-width / 4, 0, 0);
     rotateX(frameCount*0.05);
     rotateY(frameCount*0.01);
     rotateZ(frameCount*0.05);
-    texture(img);
+    texture(imgB);
     box(height/3);
+    pop();
+
+    //creation of the second 3d shape, a sphere
+    translate(width / 4, 0, 0);
+    rotateY(-frameCount*0.05);
+    texture(imgS);
+    sphere(120);
     noStroke();
   }
 
+  //runs when the window is resized, to place everything accordingly to the scale
   function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     btn.position(width/2.15,height*0.92);
     p.position(width/2.1,height*0.08);
   }
 
+  //the function that cycles a from 0 to 2 
   function lightToggle() {
     if(a === 2){
       a = 0;
